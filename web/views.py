155d0b9836c9ca7cf7d0a2ab96451a9cd4da83a4
Lisandro <lisandro_luna@ymail.com>
2020-08-31 22:17:40 -0300

    Se agrega template.

--- web/views.py
+++ web/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     """The home page for web"""
-    return render(request, 'web/index.html')
+    return render(request, 'web/home.html')
 
 
