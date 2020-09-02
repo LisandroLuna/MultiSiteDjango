447b9fec7ec62d293bb1a3d3b5dbd56eac1d509d
Lisandro <lisandro_luna@ymail.com>
2020-09-02 13:58:58 -0300

    Se cargan elementos de Front End.

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,13 @@
 """Defines URL patterns for web."""
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     #Home page
-    url('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Albums page
+    path('albums/', views.albums, name='albums'),
+    # Detail page for a single album
+    path('albumdetail/<int:album_id>/', views.albumdetail, name='albumdetail'),
 ]
 
--- web/views.py
+++ web/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Album
 
 
 def index(request):
     """The home page for web"""
     return render(request, 'web/home.html')
 
+
+def albums(request):
+    """The albums page"""
+    albums = Album.objects.order_by('year')
+    context = {'albums': albums}
+    return render(request, 'web/albums.html', context)
+
+
+def albumdetail(request, album_id):
+    """Show a single album"""
+    album = Album.objects.get(id=album_id)
+    songs = album.song_set.order_by('id')
+    context = {'album': album, 'songs': songs}
+    return render(request, 'web/albumdetail.html', context)
 
